7e9b7959bab264adbe3804f52a67e4e968a9aebf
dn200978lak <dn200978lak@gmail.com>
2015-07-31 18:04:26 +0300

    spring_demo

---
+++ src/main/java/com/mkyong/common/App.java
@@ -0,0 +1,26 @@
+package com.mkyong.common;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import com.mkyong.customer.dao.CustomerDAO;
+import com.mkyong.customer.model.Customer;
+
+/**
+ * Created by alexandr on 31.07.15.
+ * {@link http://www.mkyong.com/spring/maven-spring-jdbc-example/}
+ * ***************************************************************
+ * Spring + JDBC example
+ */
+public class App {
+
+    public static void main(String[] args){
+    	ApplicationContext context = new ClassPathXmlApplicationContext("Spring-Module.xml");
+    	 
+        CustomerDAO    customerDAO = (CustomerDAO) context.getBean("customerDAO");
+        Customer          customer = new Customer(1, "mkyong",28);
+//        customerDAO.insert(customer);
+
+        System.out.println( customerDAO.findByCustomerId(1) );
+    }
+
+}
---
+++ src/main/java/com/mkyong/customer/model/Customer.java
@@ -0,0 +1,39 @@
+package com.mkyong.customer.model;
+
+public class Customer {
+
+	public Customer(int custId, String name, int age) {
+		this.custId = custId;
+		this.name   = name;
+		this.age    = age;
+	}
+
+
+    public void setCustId(int custId) {
+        this.custId = custId;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+	public int getCustId() {
+		return custId;
+	}
+	public String getName() {
+		return name;
+	}
+	public int getAge() {
+		return age;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [age=" + age + ", custId=" + custId + ", name=" + name + "]";
+	}
+
+    int  custId;
+    String name;
+    int     age;
+}
