3d60ad8e13194e809f4d9127cc01e9d354230a89
dn200978lak <dn200978lak@gmail.com>
2015-08-11 16:02:37 +0300

    miratex-master

--- Task/src/test/java/com/miratex/my/Source2.java
+++ Task/src/test/java/com/miratex/my/Source2.java
@@ -15,21 +15,46 @@ public class Source2 {
 }
 
 
-class Pool {
+class Pools {
 
-    public Pool(int max){
+    public Pools(int max){
         actives = 0;
         in = new ArrayList<>();
-        reserv = new ArrayList<>(max);
-        active = new ArrayList<>(max);
+        available = new ArrayList<>(max);
+        using = new ArrayList<>();
+
+        available.add(new Pool(1, "1"));
+        available.add(new Pool(2, "2"));
+        available.add(new Pool(3, "3"));
+        available.add(new Pool(4, "4"));
+        available.add(new Pool(5, "5"));
+        available.add(new Pool(6, "6"));
     }
 
-//    public List get() {
-//    }
+    public Pool getPool() {
+        using.add(available.get(actives));
+        available.remove(actives);
+        actives++;
+        return (Pool) using.get(actives-1);
+    }
+    public void returnPool() {
+        available.add(using.get(actives));
+        using.remove(actives);
+        actives--;
+    }
 
     private int actives;
     private List in;
-    private List reserv;
-    private List active;
+    private List available;
+    private List using;
+    private Pool pool;
+}
 
+class Pool {
+    public Pool(int number, String value){
+        this.number = number;
+        this.value = value;
+    }
+    private int number;
+    private String value;
 }
