97e22afde915b9bc17a487ff46d7adf9d99f1436
dn200978lak <dn200978lak@gmail.com>
2015-08-19 16:19:00 +0300

    miratex-master

--- example.sql
+++ example.sql
@@ -1,3 +1,15 @@
+-- (Spring 3.1 + JPA + DBUnit, Gradle, Maven) http://george-zalizko.blogspot.com/2012/10/spring-31-jpa-dbunit-gradle-maven.html
+-- (Класс преподавателя...) https://github.com/wizardjedi/my-spring-learning/wiki/Работа-с-базами-данных-на-основе-jpa
+-- (Первичные ключи..) http://www.softtime.ru/bookphp/gl12_4.php
+-- (Spring Data JPA. Работа с БД. Часть 1) http://devcolibri.com/3966
+--                                         http://devcolibri.com/4149
+-- (Примеры приложений..) http://habrahabr.ru/post/232381/
+-- (Распределённая система кеша ehcache для приложений любого уровня) http://habrahabr.ru/post/25140/
+-- (Стратегия кеширования в приложении) http://habrahabr.ru/post/168725/
+-- (Hibernate кэширование на практике и заключение (Ч5)) http://dr-magic.blogspot.com/2010/01/hibernate-5.html
+-- (ZK+Spring 3+Hibernate: две головы хорошо, а три лучше) http://habrahabr.ru/post/129189/
+-- (Как работает аннотация @Inject?) https://toster.ru/q/44878
+
 CREATE TABLE `Teacher` (
   `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255) DEFAULT NULL,
